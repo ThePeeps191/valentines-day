6f904186ffde6ab9a429f9fa7291353e30373b7e
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-02-08 14:12:29 -0700

    Update navig.js

--- navig.js
+++ navig.js
@@ -1,4 +1,4 @@
-document.write(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
+document.write(`<nav class="navbar navbar-expand-lg navbar-light bg-danger">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
